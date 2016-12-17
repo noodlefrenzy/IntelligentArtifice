086b66e190cf45493d255b2a263966831e739faf
Mike Lanzetta <milanz@microsoft.com>
2016-12-17 10:09:42 -0800

    Update pelican conf to generate feed.

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,7 +25,7 @@ PLUGIN_PATHS = ['../pelican-plugins']
 PLUGINS = ['sitemap', 'gravatar']
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
